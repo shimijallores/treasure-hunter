49570ea9c2c7677a6231b0de413e5f0c3fd2339a
zielww <118595018+zielww@users.noreply.github.com>
2025-09-14 20:55:21 +0800

    added basic ui

--- main.js
+++ main.js
@@ -8,10 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
     "https://assets.codepen.io/6201207/codepen-iso-tilesheet.png"
   );
 
+  const roundDisplay = document.querySelector("#round");
+  const scoreDisplay = document.querySelector("#score");
+  const timerDisplay = document.querySelector("#timer");
+  const lifeDisplay = document.querySelector("#life");
   let score = 0;
   let round = 1;
 
   function startRound() {
+    world.buildMap();
+
     let treasures = [];
     let deathBomb = [];
     let extraLife = [];
@@ -27,6 +33,10 @@ document.addEventListener("DOMContentLoaded", () => {
         deathBomb : ${deathBomb} 
         extraLife : ${extraLife} 
       `);
+
+    roundDisplay.textContent = `Round: ${round}`;
+    scoreDisplay.textContent = `Score: ${score}`;
+    lifeDisplay.textContent = `Life: ${lives}`;
 
     // Main event listener
     world.canvas.ondblclick = (e) => {
@@ -50,12 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
         // Treasure
         world.tileMap[row][col] = 4;
         minedCells.push(coordinates);
+        score++;
+        checkMinedTreasures();
       } else if (arraysEqual(deathBomb, coordinates)) {
         // DeathBomb (1 hit delete)
         world.tileMap[row][col] = 5;
         world.shakeCanvasContext(20, 500);
         lives = 0;
-        console.log("You Lost!");
+        console.log("You Chose the Death Bomb! BOOOOM!");
       } else if (arraysEqual(extraLife, coordinates)) {
         // Extra Life
         world.tileMap[row][col] = 2;
@@ -67,46 +79,74 @@ document.addEventListener("DOMContentLoaded", () => {
         world.shakeCanvasContext(3);
         lives--;
         minedCells.push(coordinates);
-        if (lives == 0) {
-          console.log("You Lost!");
-        }
+        nextRound();
       }
 
-      console.log(row, col);
-      console.log(`Remaining lives: ${lives}`);
       console.log(`Mined Cells: ${minedCells}`);
+      roundDisplay.textContent = `Round: ${round}`;
+      scoreDisplay.textContent = `Score: ${score}`;
+      lifeDisplay.textContent = `Life: ${lives}`;
     };
+
+    function nextRound() {
+      if (lives == 0) {
+        // Check if a treasure is found, if found, move on to the next round, else notify the player lost
+        treasures.forEach((treasure) => {
+          if (minedCells.some((cell) => arraysEqual(cell, treasure))) {
+            // Next round
+            startRound();
+            round++;
+          } else {
+            console.log("You Lost!");
+          }
+        });
+      }
+    }
+
+    function checkMinedTreasures() {
+      // Check if both treasure items are in the mineCells, then proceed to the next round
+      treasures.forEach((treasure) => {
+        if (
+          minedCells.length > 1 &&
+          minedCells.some((cell) => arraysEqual(cell, treasure))
+        ) {
+          // Next round
+          startRound();
+          round++;
+        }
+      });
+    }
   }
 
   startRound();
-
-  // Utility Functions
-  function generateCoordinates() {
-    let takenPositions = [];
-
-    let treasure1 = generateUniqueCell(takenPositions);
-    let treasure2 = generateUniqueCell(takenPositions);
-    let deathBomb = generateUniqueCell(takenPositions);
-    let extraLife = generateUniqueCell(takenPositions);
-
-    return [[treasure1, treasure2], deathBomb, extraLife];
-  }
-
-  function generateRandomCell() {
-    return [Math.floor(Math.random(0) * 3), Math.floor(Math.random(0) * 3)];
-  }
-
-  function generateUniqueCell(takenPositions) {
-    let cell;
-    do {
-      cell = generateRandomCell();
-    } while (takenPositions.some((item) => arraysEqual(item, cell)));
-    takenPositions.push(cell);
-    return cell;
-  }
-
-  function arraysEqual(a, b) {
-    if (a.length !== b.length) return false;
-    return a.every((val, i) => val === b[i]);
-  }
 });
+
+// Utility Functions
+function generateCoordinates() {
+  let takenPositions = [];
+
+  let treasure1 = generateUniqueCell(takenPositions);
+  let treasure2 = generateUniqueCell(takenPositions);
+  let deathBomb = generateUniqueCell(takenPositions);
+  let extraLife = generateUniqueCell(takenPositions);
+
+  return [[treasure1, treasure2], deathBomb, extraLife];
+}
+
+function generateRandomCell() {
+  return [Math.floor(Math.random(0) * 3), Math.floor(Math.random(0) * 3)];
+}
+
+function generateUniqueCell(takenPositions) {
+  let cell;
+  do {
+    cell = generateRandomCell();
+  } while (takenPositions.some((item) => arraysEqual(item, cell)));
+  takenPositions.push(cell);
+  return cell;
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length) return false;
+  return a.every((val, i) => val === b[i]);
+}
